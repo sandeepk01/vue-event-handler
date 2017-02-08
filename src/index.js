90979f3eca39fa27b96998f10a872fb801a8b89e
sandeepk01 <sandeep.bhat.vns@gmail.com>
2017-02-08 20:18:06 +0530

    Listen once and remove all added

--- src/index.js
+++ src/index.js
@@ -1,41 +1,45 @@
 function plugin(Vue) {
-
     if (plugin.installed) {
         return;
     }
-
     const events = new Vue({
-        methods: {
-            fire(name, data = null) {
-                this.emit(name, data)
-            },
-            emit(name, data = null) {
-                this.$emit(name, data);
-            },
-            listen(name, cb) {
-                this.on(name, cb)
-            },
-            on(name, cb) {
-                this.$on(name, cb);
-            },
-			off(name, cb) {
-				this.$off(name, cb);
-			},
-			unlisten(name, cb) {
-				this.off(name, cb)
-			}
-        }
+	    methods: {
+		    fire (name, data = null) {
+			    this.emit(name, data)
+		    },
+		    emit (name, data = null) {
+			    this.$emit(name, data)
+		    },
+		    listen (name, cb) {
+			    this.on(name, cb)
+		    },
+		    listenOnce (name, cb) {
+			    this.once(name, cb)
+		    },
+		    on (name, cb) {
+			    this.$on(name, cb)
+		    },
+		    once (name, cb) {
+			    this.$once(name, cb)
+		    },
+		    off (name, cb) {
+			    this.$off(name, cb)
+		    },
+		    unlisten (name, cb) {
+			    this.off(name, cb)
+		    },
+		    removeAll () {
+			    this.$off()
+		    }
+	    }
     });
-
     Object.defineProperty(Vue.prototype, '$events', {
-        get() {
-            return events;
-        }
+	    get() {
+		    return events;
+	    }
     });
 }
-
 if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(plugin);
+	window.Vue.use(plugin);
 }
-
 export default plugin;
