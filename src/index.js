ef0668bda01115baf73bb0ec343b94f24de86441
hYp3rIoS <sandeep.bhat.vns@gmail.com>
2019-06-07 09:35:34 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,45 +1,78 @@
 function plugin(Vue) {
+
     if (plugin.installed) {
         return;
     }
+	var evmap = {}
     const events = new Vue({
-	    methods: {
-		    fire (name, data = null) {
-			    this.emit(name, data)
-		    },
-		    emit (name, data = null) {
-			    this.$emit(name, data)
-		    },
-		    listen (name, cb) {
-			    this.on(name, cb)
-		    },
-		    listenOnce (name, cb) {
-			    this.once(name, cb)
-		    },
-		    on (name, cb) {
-			    this.$on(name, cb)
-		    },
-		    once (name, cb) {
-			    this.$once(name, cb)
-		    },
-		    off (name, cb) {
-			    this.$off(name, cb)
-		    },
-		    unlisten (name, cb) {
-			    this.off(name, cb)
-		    },
-		    removeAll () {
-			    this.$off()
-		    }
-	    }
-    });
+        methods: {
+            fire (name, data = null) {
+                this.emit(name, data)
+            },
+            emit (name, data = null) {
+                this.$emit(name, data)
+            },
+            listen (name, cb) {
+                this.on(name, cb)
+            },
+			listenOnce (name, cb) {
+                this.once(name, cb)
+            },
+            on (name, cb) {
+				evmap[name] = cb
+				this.$on(name, cb)
+            },
+			once (name, cb) {
+				evmap[name] = cb
+				this.$once(name, cb)
+            },
+			off (name, cb) {
+				if (!cb) {
+					cb = evmap[name]
+				}
+				this.$off(name, cb)
+			},
+			unlisten (name, cb) {
+				this.off(name, cb)
+			},
+			removeAll () {
+				this.$off()
+			}
+        }
+    })
+	
+	Vue.mixin({
+		beforeCreate() {
+			if (typeof this.$options.events !== 'object') return
+			var eventMap = {}
+			for (var key in this.$options.events) {
+				eventMap[key] = this.$options.events[key].bind(this)
+			}
+			this.$once('hook:beforeMount', () => {
+				for (var key in eventMap) {
+					events.$on(key, eventMap[key])
+				}
+			})
+			this.$once('hook:beforeDestroy', () => {
+				for (var key in eventMap) {
+					events.$off(key, eventMap[key])
+				}
+				eventMap = null
+			})
+		}
+	})
+	
+	Vue.events = events
+	
     Object.defineProperty(Vue.prototype, '$events', {
-	    get() {
-		    return events;
-	    }
+        get() {
+            return events;
+        }
     });
 }
+
 if (typeof window !== 'undefined' && window.Vue) {
-	window.Vue.use(plugin);
+    window.Vue.use(plugin);
 }
+
 export default plugin;
